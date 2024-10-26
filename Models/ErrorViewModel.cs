b73ba39934aa5ca3968017d994939768187ec529
shodiqbasiru <shodiqbasiru@gmail.com>
2024-10-26 10:17:16 +0700

    :tada: Initial Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace EduBasPlatform.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
